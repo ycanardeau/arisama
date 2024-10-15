fa238d1fc53efb7e62e3c8c865337c1f7ee4c6a3
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2024-10-15 23:38:03 +0900

    Add files

---
+++ Arisama/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace Arisama;
+
+internal abstract record VendingMachineStateContext
+{
+    public sealed record Idle : VendingMachineStateContext;
+
+    public sealed record CoinInserted : VendingMachineStateContext;
+
+    public sealed record ProductChosen : VendingMachineStateContext;
+
+    public sealed record ChangeReturned : VendingMachineStateContext;
+
+    public sealed record ProductDispensed : VendingMachineStateContext;
+}
+
+internal abstract record VendingMachineState
+{
+    public sealed record Idle : VendingMachineState<VendingMachineStateContext.Idle>;
+
+    public sealed record CoinInserted : VendingMachineState<VendingMachineStateContext.CoinInserted>;
+
+    public sealed record ProductChosen : VendingMachineState<VendingMachineStateContext.ProductChosen>;
+
+    public sealed record ChangeReturned : VendingMachineState<VendingMachineStateContext.ChangeReturned>;
+
+    public sealed record ProductDispensed : VendingMachineState<VendingMachineStateContext.ProductDispensed>;
+}
+
+internal abstract record VendingMachineState<TContext> : VendingMachineState
+    where TContext : VendingMachineStateContext;
+
+internal class VendingMachine
+{
+    private readonly List<VendingMachineState> _states = [];
+    public IReadOnlyCollection<VendingMachineState> States => _states.AsReadOnly();
+
+    private VendingMachine() { }
+
+    private void FromTo<TFrom, TTo>(Func<TFrom, TTo> callback)
+        where TFrom : VendingMachineState
+        where TTo : VendingMachineState
+    {
+        Console.WriteLine($"Transitioning from {typeof(TFrom).Name} to {typeof(TTo).Name}.");
+
+        if (States.Last() is not TFrom from)
+        {
+            Console.WriteLine($"Invalid transition from {typeof(TFrom).Name} to {typeof(TTo).Name}.");
+            return;
+        }
+
+        _states.Add(callback(from));
+
+        Console.WriteLine($"Transitioned from {typeof(TFrom).Name} to {typeof(TTo).Name}.");
+    }
+
+    public static VendingMachine Create()
+    {
+        var vendingMachine = new VendingMachine();
+
+        vendingMachine._states.Add(new VendingMachineState.Idle());
+
+        return vendingMachine;
+    }
+
+    public void InsertCoin(int amount)
+    {
+        FromTo<VendingMachineState.Idle, VendingMachineState.CoinInserted>(from => new VendingMachineState.CoinInserted());
+    }
+
+    public void ChooseProduct()
+    {
+        FromTo<VendingMachineState.CoinInserted, VendingMachineState.ProductChosen>(from => new VendingMachineState.ProductChosen());
+    }
+
+    public void ReturnChange()
+    {
+        FromTo<VendingMachineState.CoinInserted, VendingMachineState.Idle>(from => new VendingMachineState.Idle());
+    }
+
+    public void DispenseProduct()
+    {
+        FromTo<VendingMachineState.ProductChosen, VendingMachineState.ProductDispensed>(from => new VendingMachineState.ProductDispensed());
+    }
+}
+
+internal static class Program
+{
+    public static void Main()
+    {
+        var vendingMachine = VendingMachine.Create();
+
+        vendingMachine.InsertCoin(amount: 100);
+
+        vendingMachine.ChooseProduct();
+
+        vendingMachine.DispenseProduct();
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Arisama
+
+## References
+
+-   [dotnet-state-machine/stateless: A simple library for creating state machines in C# code](https://github.com/dotnet-state-machine/stateless)
+-   [statelyai/xstate: Actor-based state management &amp; orchestration for complex app logic.](https://github.com/statelyai/xstate)
+-   [Go Design Pattern — State Design Pattern — Low-Level Design of Vending Machine | by Akhilesh Mahajan | Medium](https://akhileshmj.medium.com/go-design-pattern-state-design-pattern-low-level-design-of-vending-machine-bad8b092cd94)
