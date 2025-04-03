a2ae493a645a858eb7ab5df416f97b0facbc7a7d
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-04-03 23:27:22 +0900

    Create migration

---
+++ Experimental/WebApp/Modules/CivilRegistration/WebApp.CivilRegistration.MigrationService/Migrations/20250403142712_DeathCertificate.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.CivilRegistration.MigrationService.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeathCertificate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeathCertificates",
+                schema: "WebApp_CivilRegistration",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DeceasedId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeathCertificates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DeathCertificates_Persons_DeceasedId",
+                        column: x => x.DeceasedId,
+                        principalSchema: "WebApp_CivilRegistration",
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeathCertificates_DeceasedId",
+                schema: "WebApp_CivilRegistration",
+                table: "DeathCertificates",
+                column: "DeceasedId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeathCertificates",
+                schema: "WebApp_CivilRegistration");
+        }
+    }
+}
