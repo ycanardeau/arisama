ef9f47bb549b4ebc92c5efacb9e4cacee1a4ecb0
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-04-12 11:14:50 +0900

    Create migration

---
+++ Experimental/WebApp/Modules/CivilRegistration/WebApp.CivilRegistration.MigrationService/Migrations/20250412021431_AddGuidToCertificates.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.CivilRegistration.MigrationService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGuidToCertificates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Guid",
+                schema: "WebApp_CivilRegistration",
+                table: "MarriageCertificates",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Guid",
+                schema: "WebApp_CivilRegistration",
+                table: "DivorceCertificates",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Guid",
+                schema: "WebApp_CivilRegistration",
+                table: "DeathCertificates",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Guid",
+                schema: "WebApp_CivilRegistration",
+                table: "MarriageCertificates");
+
+            migrationBuilder.DropColumn(
+                name: "Guid",
+                schema: "WebApp_CivilRegistration",
+                table: "DivorceCertificates");
+
+            migrationBuilder.DropColumn(
+                name: "Guid",
+                schema: "WebApp_CivilRegistration",
+                table: "DeathCertificates");
+        }
+    }
+}
