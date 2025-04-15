4f22e16bd747b4cd8907aed9832aec38086abd13
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-04-15 22:31:03 +0900

    Create migration

--- Experimental/WebApp/Modules/CivilRegistration/WebApp.CivilRegistration.MigrationService/Migrations/20250415133055_OwnsOne.Designer.cs
+++ Experimental/WebApp/Modules/CivilRegistration/WebApp.CivilRegistration.MigrationService/Migrations/20250415133055_OwnsOne.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApp.CivilRegistration.Infrastructure.Persistence;
 
@@ -10,9 +11,11 @@ using WebApp.CivilRegistration.Infrastructure.Persistence;
 namespace WebApp.CivilRegistration.MigrationService.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250415133055_OwnsOne")]
+    partial class OwnsOne
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
