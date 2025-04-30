5d1d0a23431ab9a45dec53a9a192feda5b818a19
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-04-30 23:31:08 +0900

    Create MarriageCertificateGrain

---
+++ Experimental/WebApp/Modules/CivilRegistration.Orleans/WebApp.CivilRegistration.Orleans.Infrastructure/Grains/MarriageCertificateGrain.cs
@@ -0,0 +1,7 @@
+using WebApp.CivilRegistration.Orleans.Infrastructure.Grains.Abstractions;
+
+namespace WebApp.CivilRegistration.Orleans.Infrastructure.Grains;
+
+internal class MarriageCertificateGrain : Grain, IMarriageCertificateGrain
+{
+}
