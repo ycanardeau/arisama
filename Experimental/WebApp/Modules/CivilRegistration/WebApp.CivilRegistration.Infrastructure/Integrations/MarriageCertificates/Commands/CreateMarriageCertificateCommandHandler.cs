79b054f081f8c328e7a316d650d0ae7fd6258262
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-09-19 20:15:01 +0900

    Refactor CreateMarriageCertificateCommandHandler

--- Experimental/WebApp/Modules/CivilRegistration/WebApp.CivilRegistration.Infrastructure/Integrations/MarriageCertificates/Commands/CreateMarriageCertificateCommandHandler.cs
+++ Experimental/WebApp/Modules/CivilRegistration/WebApp.CivilRegistration.Infrastructure/Integrations/MarriageCertificates/Commands/CreateMarriageCertificateCommandHandler.cs
@@ -4,6 +4,7 @@ using Nut.Results;
 using WebApp.CivilRegistration.Contracts.MarriageCertificates.Commands;
 using WebApp.CivilRegistration.Contracts.MarriageCertificates.Dtos;
 using WebApp.CivilRegistration.Domain.MarriageCertificates.Entities;
+using WebApp.CivilRegistration.Domain.Persons.Entities;
 using WebApp.CivilRegistration.Domain.Persons.ValueObjects;
 using WebApp.CivilRegistration.Infrastructure.Persistence;
 
@@ -11,27 +12,22 @@ namespace WebApp.CivilRegistration.Infrastructure.Integrations.MarriageCertifica
 
 internal class CreateMarriageCertificateCommandHandler(ApplicationDbContext dbContext) : IRequestHandler<CreateMarriageCertificateCommand, Result<CreateMarriageCertificateResponseDto>>
 {
-	public async Task<Result<CreateMarriageCertificateResponseDto>> Handle(CreateMarriageCertificateCommand request, CancellationToken cancellationToken)
+	private async Task<Result<Person>> GetPersonAsync(PersonId personId, CancellationToken cancellationToken)
 	{
-		var husband = await dbContext.Persons
+		var person = await dbContext.Persons
 			.Include(x => x.MaritalStateMachine)
-			.SingleOrDefaultAsync(x => x.Id == new PersonId(request.HusbandId), cancellationToken);
+			.SingleOrDefaultAsync(x => x.Id == personId, cancellationToken);
 
-		if (husband is null)
-		{
-			return Result.Error<CreateMarriageCertificateResponseDto>(new InvalidOperationException($"Person {request.HusbandId} not found"));
-		}
+		return person is null
+			? Result.Error<Person>(new InvalidOperationException($"Person {personId} not found"))
+			: person;
+	}
 
-		var wife = await dbContext.Persons
-			.Include(x => x.MaritalStateMachine)
-			.SingleOrDefaultAsync(x => x.Id == new PersonId(request.WifeId), cancellationToken);
-
-		if (wife is null)
-		{
-			return Result.Error<CreateMarriageCertificateResponseDto>(new InvalidOperationException($"Person {request.WifeId} not found"));
-		}
-
-		return await MarriageCertificate.Create(new CreateCommand(husband, wife))
+	public Task<Result<CreateMarriageCertificateResponseDto>> Handle(CreateMarriageCertificateCommand request, CancellationToken cancellationToken)
+	{
+		return GetPersonAsync(new PersonId(request.HusbandId), cancellationToken)
+			.Combine(x => GetPersonAsync(new PersonId(request.WifeId), cancellationToken))
+			.FlatMap(x => MarriageCertificate.Create(new CreateCommand(x.Item1, x.Item2)))
 			.Tap(x => dbContext.MarriageCertificates.Add(x))
 			.Tap(x => dbContext.SaveChangesAsync(cancellationToken))
 			.Map(x => new CreateMarriageCertificateResponseDto(Id: x.Id.Value));
