d9c9e2fe1fa4e215cd42887c4dd258149ef94f3f
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2026-09-22 15:27:14 +0900

    Return specific domain errors as problem details

--- Experimental/WebApp/Modules/CivilRegistration.Orleans/WebApp.CivilRegistration.Orleans.Endpoints/ResultExtensions.cs
+++ Experimental/WebApp/Modules/CivilRegistration.Orleans/WebApp.CivilRegistration.Orleans.Endpoints/ResultExtensions.cs
@@ -2,6 +2,13 @@ namespace WebApp.CivilRegistration.Orleans.Endpoints;
 
 internal static class ResultExtensions
 {
+	private static IResult Problem(int statusCode, string code, string detail) =>
+		TypedResults.Problem(
+			statusCode: statusCode,
+			detail: detail,
+			extensions: new Dictionary<string, object?> { ["code"] = code }
+		);
+
 	public static IResult ToMinimalApiResult<T>(
 		this Result<T, CivilRegistrationOrleansError> result
 	)
@@ -9,9 +16,19 @@ internal static class ResultExtensions
 		return result.Fold(
 			onOk: value => TypedResults.Ok(value),
 			onError: error =>
-				error.Match<IResult>(
-					onAlreadyInitialized: _ => TypedResults.Conflict(),
-					onInvalidMaritalState: _ => TypedResults.UnprocessableEntity()
+				error.Match(
+					onAlreadyInitialized: _ =>
+						Problem(
+							StatusCodes.Status409Conflict,
+							nameof(CivilRegistrationOrleansError.AlreadyInitialized),
+							"The marital state machine has already been initialized."
+						),
+					onInvalidMaritalState: _ =>
+						Problem(
+							StatusCodes.Status422UnprocessableEntity,
+							nameof(CivilRegistrationOrleansError.InvalidMaritalState),
+							"The requested transition is not valid from the current marital state."
+						)
 				)
 		);
 	}
--- Experimental/WebApp/Modules/CivilRegistration/WebApp.CivilRegistration.Endpoints/ResultExtensions.cs
+++ Experimental/WebApp/Modules/CivilRegistration/WebApp.CivilRegistration.Endpoints/ResultExtensions.cs
@@ -2,20 +2,65 @@ namespace WebApp.CivilRegistration.Endpoints;
 
 internal static class ResultExtensions
 {
+	private static IResult Problem(int statusCode, string code, string detail) =>
+		TypedResults.Problem(
+			statusCode: statusCode,
+			detail: detail,
+			extensions: new Dictionary<string, object?> { ["code"] = code }
+		);
+
+	private static IResult Unprocessable(string code, string detail) =>
+		Problem(StatusCodes.Status422UnprocessableEntity, code, detail);
+
+	private static IResult NotFound(string code, string detail) =>
+		Problem(StatusCodes.Status404NotFound, code, detail);
+
 	public static IResult ToMinimalApiResult<T>(this Result<T, CivilRegistrationError> result)
 	{
 		return result.Fold(
 			onOk: value => TypedResults.Ok(value),
 			onError: error =>
-				error.Match<IResult>(
-					onSameIndividual: _ => TypedResults.UnprocessableEntity(),
-					onSameSexMarriage: _ => TypedResults.UnprocessableEntity(),
-					onIneligibleHusband: _ => TypedResults.UnprocessableEntity(),
-					onIneligibleWife: _ => TypedResults.UnprocessableEntity(),
-					onNotMarriageable: _ => TypedResults.UnprocessableEntity(),
-					onInvalidMaritalState: _ => TypedResults.UnprocessableEntity(),
-					onPersonNotFound: _ => TypedResults.NotFound(),
-					onMarriageCertificateNotFound: _ => TypedResults.NotFound()
+				error.Match(
+					onSameIndividual: _ =>
+						Unprocessable(
+							nameof(CivilRegistrationError.SameIndividual),
+							"A marriage requires two individuals."
+						),
+					onSameSexMarriage: _ =>
+						Unprocessable(
+							nameof(CivilRegistrationError.SameSexMarriage),
+							"Same-sex marriage is not allowed in Japan as of writing."
+						),
+					onIneligibleHusband: _ =>
+						Unprocessable(
+							nameof(CivilRegistrationError.IneligibleHusband),
+							"The person cannot take the role of husband."
+						),
+					onIneligibleWife: _ =>
+						Unprocessable(
+							nameof(CivilRegistrationError.IneligibleWife),
+							"The person cannot take the role of wife."
+						),
+					onNotMarriageable: _ =>
+						Unprocessable(
+							nameof(CivilRegistrationError.NotMarriageable),
+							"The person is not of marriageable age."
+						),
+					onInvalidMaritalState: _ =>
+						Unprocessable(
+							nameof(CivilRegistrationError.InvalidMaritalState),
+							"The requested transition is not valid from the current marital state."
+						),
+					onPersonNotFound: _ =>
+						NotFound(
+							nameof(CivilRegistrationError.PersonNotFound),
+							"The referenced person does not exist."
+						),
+					onMarriageCertificateNotFound: _ =>
+						NotFound(
+							nameof(CivilRegistrationError.MarriageCertificateNotFound),
+							"The referenced marriage certificate does not exist."
+						)
 				)
 		);
 	}
